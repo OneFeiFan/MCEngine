37068a972f1f58c66f722c3900954f70d4885e76
OneFeiFan <3022504358@qq.com>
2023-07-21 21:08:51 +0800

    优化代码

--- app/src/main/jni/loader/main.cpp
+++ app/src/main/jni/loader/main.cpp
@@ -6,76 +6,63 @@
 #include <unistd.h>
 #include <iostream>
 #include <cstring>
-const char *linkerName;
+
+//const char *linkerName;
 void *symbol = nullptr;
 JavaVM *loaderVM = nullptr;
-void * loaderPtr = nullptr;
-char *jstringToChar(JNIEnv *env, jstring jstr)
-{
+void *loaderPtr = nullptr;
+
+char *jstringToChar(JNIEnv *env, jstring jstr) {
     char *rtn = nullptr;
-    jclass clsstring = env->FindClass("java/lang/String");
+    jclass class_string = env->FindClass("java/lang/String");
     jstring strencode = env->NewStringUTF("utf-8");
-    jmethodID mid = env->GetMethodID(clsstring, "getBytes", "(Ljava/lang/String;)[B");
-    auto barr = (jbyteArray)env->CallObjectMethod(jstr, mid, strencode);
+    jmethodID mid = env->GetMethodID(class_string, "getBytes", "(Ljava/lang/String;)[B");
+    auto barr = (jbyteArray) env->CallObjectMethod(jstr, mid, strencode);
     jsize alen = env->GetArrayLength(barr);
     jbyte *ba = env->GetByteArrayElements(barr, JNI_FALSE);
-    if (alen > 0)
-    {
-        rtn = (char *)malloc(alen + 1);
+    if (alen > 0) {
+        rtn = (char *) malloc(alen + 1);
         memcpy(rtn, ba, alen);
         rtn[alen] = 0;
     }
     env->ReleaseByteArrayElements(barr, ba, 0);
     return rtn;
-};
+}
 
 
 extern "C"
 {
 
-    JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void * ptr)
-    {
-        freopen("/storage/emulated/0/tmp/log.txt", "w", stdout);
-        if constexpr (sizeof(void *) == 8)
-        {
-            linkerName = "linker64";
-        }
-        else
-        {
-            linkerName = "linker";
-        }
-        loaderVM = vm;
-        loaderPtr = ptr;
-        symbol = DobbySymbolResolver(linkerName, "__loader_android_dlopen_ext");
-        return JNI_VERSION_1_6;
-    }
-    JNIEXPORT void JNICALL Java_com_taolesi_mcengine_HookEngine_setDL(JNIEnv *env, jclass clazz, jstring dlpath)
-    {
-        try
-        {
-            utils::ProcessView processObj;
-            int temp = processObj.readProcess(getpid());
-            if (symbol)
-            {
-                auto *(*my_android_dlopen_ext)(const char *, int, void *, void *) = (void *(*)(const char *, int, void *, void *))symbol;
-                for (int i = 0; i < processObj.getModules().size(); ++i)
-                {
-                    if (processObj.getModules()[i].name == "libminecraftpe.so")
-                    {
-                        void *handle =  my_android_dlopen_ext(strcat(jstringToChar(env, dlpath), "!/lib/armeabi-v7a/libexample.so"), RTLD_NOW, nullptr, (void *)processObj.getModules()[i].baseAddress);
-                        //log::Toast("已取得句柄");
-                        auto (*my_JNI_OnLoad)(JavaVM *,void *)=(jint (*)(JavaVM *, void *))dlsym(handle,"JNI_OnLoad");
-                        my_JNI_OnLoad(loaderVM,loaderPtr);
-
-                        break;
-                    }
+JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *ptr) {
+    freopen("/storage/emulated/0/tmp/log.txt", "w", stdout);
+        //linkerName = "linker64";
+    loaderVM = vm;
+    loaderPtr = ptr;
+    symbol = DobbySymbolResolver("linker", "__loader_android_dlopen_ext");
+    return JNI_VERSION_1_6;
+}
+JNIEXPORT void JNICALL Java_com_taolesi_mcengine_HookEngine_setDL(JNIEnv *env,[[maybe_unused]] jclass clazz,jstring dlpath) {
+    try {
+        utils::ProcessView processObj;
+        [[maybe_unused]] int temp = processObj.readProcess(getpid());
+        if (symbol) {
+            auto *(*my_android_dlopen_ext)(const char *, int, void *, void *) = (void *(*)(const char *,int, void *,void *)) symbol;
+            for (int i = 0; i < processObj.getModules().size(); ++i) {
+                if (processObj.getModules()[i].name == "libminecraftpe.so") {
+                    void *handle = my_android_dlopen_ext(
+                            strcat(jstringToChar(env, dlpath), "!/lib/armeabi-v7a/libexample.so"),
+                            RTLD_NOW, nullptr, (void *) processObj.getModules()[i].baseAddress);
+                    //log::Toast("已取得句柄");
+                    auto(*my_JNI_OnLoad)(JavaVM *, void *) =(jint (*)(JavaVM *, void *)) dlsym(handle,"JNI_OnLoad");
+                    my_JNI_OnLoad(loaderVM, loaderPtr);
+                    break;
                 }
             }
         }
-        catch(const std::exception& e)
-        {
-            std::cerr << e.what() << '\n';
-        }
-
     }
+    catch (const std::exception &e) {
+        std::cerr << e.what() << '\n';
+    }
+
+}
 }
