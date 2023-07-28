d48801b0d747faa47b58699a94eda17599a2c8bf
OneFeiFan <3022504358@qq.com>
2023-07-28 20:43:15 +0800

    修改食物类型值的写法

--- app/src/main/java/com/taolesi/mcengine/NativeItem.java
+++ app/src/main/java/com/taolesi/mcengine/NativeItem.java
@@ -2,13 +2,25 @@ package com.taolesi.mcengine;
 
 import android.webkit.JavascriptInterface;
 
+import com.quickjs.JSObject;
+
 public class NativeItem {
+    static String saturation = " ";
     @JavascriptInterface
     public static native void createItem(String name, String icon, int index, boolean addToCategory, int type);
     @JavascriptInterface
     public static native void createFood(String name, String icon, int index, boolean addToCategory, int type, String FoodData);
     @JavascriptInterface
-    public static void createFoodItem(String name, String icon, int index, boolean addToCategory, int type, int use_duration, int nutrition ,String saturation) {
-        createFood(name, icon, index, addToCategory, type, "{\"components\": {\"minecraft:use_duration\":" + use_duration + ",\"minecraft:food\": {\"nutrition\": " + nutrition + ",\"saturation_modifier\": \""+saturation+"\"}}}");
+    public static void createFoodItem(String name, String icon, int index, boolean addToCategory, int type, JSObject foodData) {
+        switch (foodData.getInteger("saturationLevel")){
+            case 1 : saturation = "poor";break;
+            case 2 : saturation = "low";break;
+            case 3 : saturation = "normal";break;
+            case 4 : saturation = "good";break;
+            case 5 : saturation = "max";break;
+            case 6 : saturation = "supernatural";break;
+            default: saturation = "normal";break;
+        }
+        createFood(name, icon, index, addToCategory, type, "{\"components\": {\"minecraft:use_duration\":" + (foodData.contains("useDuration") ? foodData.getString("use_duration"):"32") + ",\"minecraft:food\": {\"nutrition\": " + (foodData.contains("nutrition") ? foodData.getString("nutrition"):"2") + ",\"saturation_modifier\": \""+saturation+"\"}}}");
     }
 }
