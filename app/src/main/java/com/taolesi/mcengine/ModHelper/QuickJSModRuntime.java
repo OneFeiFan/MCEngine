9f5de1cd2a1b1a6d6af5811224045195c3ad95ef
OneFeiFan <3022504358@qq.com>
2023-09-23 23:39:53 +0800

    重写注册方法，重新封装类，并可以使用对象+物品/方块名作为变量储存注册结果

--- app/src/main/java/com/taolesi/mcengine/ModHelper/QuickJSModRuntime.java
+++ app/src/main/java/com/taolesi/mcengine/ModHelper/QuickJSModRuntime.java
@@ -1,6 +1,9 @@
 package com.taolesi.mcengine.ModHelper;
 
+
 import static com.taolesi.mcengine.UsefullTools.FileTools.JsonToObjTest;
+
+import android.webkit.JavascriptInterface;
 
 import com.quickjs.JSArray;
 import com.quickjs.JSContext;
@@ -9,13 +12,13 @@ import com.quickjs.JSObject;
 import com.quickjs.JavaCallback;
 import com.quickjs.JavaVoidCallback;
 import com.quickjs.QuickJS;
+import com.taolesi.mcengine.NativeClass.NativeBlock;
 import com.taolesi.mcengine.NativeClass.NativeItem;
 import com.taolesi.mcengine.UsefullTools.Examination;
 
 public class QuickJSModRuntime {
     private QuickJS qjs;
     private JSContext context;
-
 
 
     private String mmodName;
@@ -26,22 +29,28 @@ public class QuickJSModRuntime {
     public QuickJS getQuickJS() {
         return qjs;
     }
+
     public JSContext getJSContext() {
         return context;
     }
+
     public String getModName() {
         return mmodName;
     }
+
     public String getModPath() {
         return mmodPath;
     }
+
     public String getModInfoPath() {
         return mmodInfoPath;
     }
+
     public String getJavaScriptPath() {
         return mjavaScriptPath;
     }
-    public QuickJSModRuntime (String modName, String modPath, String modInfoPath, String javaScriptPath) {
+
+    public QuickJSModRuntime(String modName, String modPath, String modInfoPath, String javaScriptPath) {
         mmodName = modName;
         mmodPath = modPath;
         mmodInfoPath = modInfoPath;
@@ -55,32 +64,120 @@ public class QuickJSModRuntime {
         defineClass();
         runJS();
     }
+
     private void addInterfaces() {
         getJSContext().addJavascriptInterface(new Loader(), "Loader");
-        getJSContext().addJavascriptInterface(new NativeItem(), "NativeItem");
+        //getJSContext().addJavascriptInterface(new NativeItem(), "NativeItem");
+        //getJSContext().addJavascriptInterface(new NativeBlock(), "NativeBlock");
         getJSContext().addJavascriptInterface(new Examination(), "Examination");
 
     }
+
     private void defineClass() {
-        getJSContext().registerClass((thisObj, args) -> {
-            JSObject item = (JSObject) args.get(0);
-            NativeItem nativeItem = new NativeItem(item.getInteger("ItemPtr"));
-            //thisObj.set("this", nativeItem);
-            thisObj.set("name", item.getString("name"));
-            thisObj.set("icon", item.getString("icon"));
-            thisObj.set("index", item.getInteger("index"));
-            thisObj.set("type", item.getInteger("type"));
+        JSObject ItemRegistry = new JSObject(getJSContext());
+        JSObject Item = new JSObject(getJSContext());
+        JSObject BlockRegistry = new JSObject(getJSContext());
+        JSObject Block = new JSObject(getJSContext());
+        ItemRegistry.registerJavaMethod((receiver, args) -> {
+            String name = args.getString(0), iconName = args.getString(1);
+            int index = args.getInteger(2), category = args.getInteger(3);
+            NativeItem.createItem(name, iconName, index, category);
+            Item.set(name, iconName);
+        }, "createBaseItem");
 
-            JSFunction OnItemUsed = new JSFunction(getJSContext(), (receiver, temp) -> {
-                NativeItem.getCallbacks().put((JSFunction) temp.get(0), getJSContext());
-            });
-            thisObj.set("addOnItemUsedCallback", OnItemUsed);
+        ItemRegistry.registerJavaMethod((receiver, args) -> {
+            String name = args.getString(0), iconName = args.getString(1);
+            int index = args.getInteger(2), category = args.getInteger(3);
+            JSObject foodData = args.getObject(4);
+            String saturation = "normal";
+            try {//此处的try是为了防止foodData里面get不到键
+                switch (foodData.getInteger("saturationLevel")) {
+                    case 1:
+                        saturation = "poor";
+                        break;
+                    case 2:
+                        saturation = "low";
+                        break;
+                    case 4:
+                        saturation = "good";
+                        break;
+                    case 5:
+                        saturation = "max";
+                        break;
+                    case 6:
+                        saturation = "supernatural";
+                        break;
+                    default:
+                        saturation = "normal";
+                        break;
+                }
+            } catch (Exception e) {
+                Loader.Toast(String.valueOf(e));
+            }
+            NativeItem.createFood(name, iconName, index, category, "{\"components\": {\"minecraft:food\": {\"nutrition\": " + (foodData.contains("nutrition") ? foodData.getString("nutrition") : "2") + ",\"saturation_modifier\": \"" + saturation + "\"}," + "\"minecraft:use_duration\":" + (foodData.contains("useDuration") ? foodData.getString("useDuration") : "32") + "}}");
+            Item.set(name, iconName);
+        }, "createFoodItem");
+        ItemRegistry.registerJavaMethod((receiver, args) -> {
+            String name = args.getString(0), iconName = args.getString(1);
+            int index = args.getInteger(2), category = args.getInteger(3);
+            JSObject extraData = args.getObject(4);
+            String tier = "Wood";
+            try {
+                switch (extraData.getString("base")) {
+                    case "Iron":
+                        tier = "Iron";
+                        break;
+                    case "Stone":
+                        tier = "Stone";
+                        break;
+                    case "Gold":
+                        tier = "Gold";
+                        break;
+                    case "Diamond":
+                        tier = "Diamond";
+                        break;
+                    case "Netherite":
+                        tier = "Netherite";
+                        break;
+                    default:
+                        tier = "Wood";
+                        break;
+                }
+            } catch (Exception e) {
+                Loader.Toast(String.valueOf(e));
+            }
+            NativeItem.createSword(name, iconName, index, category, tier, extraData.contains("durability") ? extraData.getInteger("durability") : 0, extraData.contains("damage") ? extraData.getInteger("damage") : 0);
+            Item.set(name, iconName);
+        }, "createSwordItem");
 
-        }, "Item");
+        BlockRegistry.registerJavaMethod((receiver, args) -> {
+            String name = args.getString(0), textureName = args.getString(1), rawMaterial = args.getString(4);
+            int index = args.getInteger(2), category = args.getInteger(3);
+            String material;
+            switch (rawMaterial) {
+                case "Wood":
+                    material = "Wood";
+                    break;
+                case "Dirt":
+                    material = "Dirt";
+                    break;
+                default:
+                    material = "Stone";
+                    break;
+            }
+            NativeBlock.createBlock(name, textureName, index, category, material);
+            Block.set(name, textureName);
+        }, "createBaseBlock");
 
 
+        getJSContext().set("ItemRegistry", ItemRegistry);
+        getJSContext().set("Item", Item);
+        getJSContext().set("BlockRegistry", BlockRegistry);
+        getJSContext().set("Block", Block);
+        new NativeItem();//初始化给native提供信息
     }
-    private void runJS () {
+
+    private void runJS() {
         getJSContext().executeScript(JsonToObjTest(getJavaScriptPath()), getModName() + ".js");
         //getJSContext().close();
         //getQuickJS().close();
--- app/src/main/java/com/taolesi/mcengine/NativeClass/NativeItem.java
+++ app/src/main/java/com/taolesi/mcengine/NativeClass/NativeItem.java
@@ -17,15 +17,18 @@ public class NativeItem {
     private long ptr;
 
     public static native void define();
+
     public static Map<JSObject, JSContext> mCallbacks = new HashMap<>();
-    public NativeItem(long NC_ItemsPtr) {
-        ptr = NC_ItemsPtr;
-        //define();
-    }
+
+//    public NativeItem(long NC_ItemsPtr) {
+//        ptr = NC_ItemsPtr;
+//        //define();
+//    }
 
     public NativeItem() {
         define();
     }
+
     public static Map<JSObject, JSContext> getCallbacks() {
         return mCallbacks;
     }
@@ -34,77 +37,81 @@ public class NativeItem {
 
     public static native long createItem(String name, String icon, int index, int type);//NC_ItemsPtr
 
-    @JavascriptInterface
-    public static JSObject createBaseItem(JSObject itemData) {
-        long ptr = createItem(itemData.getString("name"), itemData.getString("icon"), itemData.getInteger("index"), itemData.getInteger("type"));
-        itemData.set("ItemPtr", ptr);
-        return itemData;
-    }
-
     public static native long createFood(String name, String icon, int index, int type, String FoodData);//NC_ItemsPtr
 
-    public static native long createSword(String name, String icon, int index, int type, int tier, int durability, int damage);//NC_ItemsPtr
+    public static native long createSword(String name, String icon, int index, int type, String tier, int durability, int damage);//NC_ItemsPtr
 
-    @JavascriptInterface
-    public static long createFoodItem(String name, String icon, int index, int type, JSObject foodData) {
-        String saturation = "normal";
-        try {
-            switch (foodData.getInteger("saturationLevel")) {
-                case 1:
-                    saturation = "poor";
-                    break;
-                case 2:
-                    saturation = "low";
-                    break;
-                case 4:
-                    saturation = "good";
-                    break;
-                case 5:
-                    saturation = "max";
-                    break;
-                case 6:
-                    saturation = "supernatural";
-                    break;
-                default:
-                    saturation = "normal";
-                    break;
-            }
-        } catch (Exception e) {
-            Loader.Toast(String.valueOf(e));
-        }
-        return createFood(name, icon, index, type, "{\"components\": {\"minecraft:food\": {\"nutrition\": " + (foodData.contains("nutrition") ? foodData.getString("nutrition") : "2") + ",\"saturation_modifier\": \"" + saturation + "\"}," + "\"minecraft:use_duration\":" + (foodData.contains("useDuration") ? foodData.getString("useDuration") : "32") + "}}");
-    }//NC_ItemsPtr
+//    public static void createBaseItem(String name, String icon, int index, int type) {
+//        createItem(name, icon, index, type);
+//    }
+//    public static JSObject createBaseItem(JSObject itemData) {
+//        long ptr = createItem(itemData.getString("name"), itemData.getString("icon"), itemData.getInteger("index"), itemData.getInteger("type"));
+//        itemData.set("ItemPtr", ptr);
+//        return itemData;
+//    }
 
-    @JavascriptInterface
-    public static long createSwordItem(String name, String icon, int index, int type, JSObject extraData) {
-        int tier = 0;
-        try {
-            switch (extraData.getString("base")) {
-                case "Iron":
-                    tier = 1;
-                    break;
-                case "Stone":
-                    tier = 2;
-                    break;
-                case "Gold":
-                    tier = 3;
-                    break;
-                case "Diamond":
-                    tier = 4;
-                    break;
-                case "Netherite":
-                    tier = 5;
-                    break;
-                default:
-                    tier = 0;
-                    break;
-            }
-        } catch (Exception e) {
-            Loader.Toast(String.valueOf(e));
-        }
 
-        return createSword(name, icon, index, type, tier, extraData.contains("durability") ? extraData.getInteger("durability") : 0, extraData.contains("damage") ? extraData.getInteger("damage") : 0);
-    }//NC_ItemsPtr
+//    @JavascriptInterface
+//    public static void createFoodItem(String name, String icon, int index, int type, JSObject foodData) {
+//        String saturation = "normal";
+//        try {//此处的try是为了防止foodData里面get不到键
+//            switch (foodData.getInteger("saturationLevel")) {
+//                case 1:
+//                    saturation = "poor";
+//                    break;
+//                case 2:
+//                    saturation = "low";
+//                    break;
+//                case 4:
+//                    saturation = "good";
+//                    break;
+//                case 5:
+//                    saturation = "max";
+//                    break;
+//                case 6:
+//                    saturation = "supernatural";
+//                    break;
+//                default:
+//                    saturation = "normal";
+//                    break;
+//            }
+//        } catch (Exception e) {
+//            Loader.Toast(String.valueOf(e));
+//        }
+//        createFood(name, icon, index, type, "{\"components\": {\"minecraft:food\": {\"nutrition\": " + (foodData.contains("nutrition") ? foodData.getString("nutrition") : "2") + ",\"saturation_modifier\": \"" + saturation + "\"}," + "\"minecraft:use_duration\":" + (foodData.contains("useDuration") ? foodData.getString("useDuration") : "32") + "}}");
+//    }//NC_ItemsPtr
+
+//    @JavascriptInterface
+//    public static void createSwordItem(String name, String icon, int index, int type, JSObject extraData) {
+//        String tier = "Wood";
+//        try {
+//            switch (extraData.getString("base")) {
+//                case "Iron":
+//                    tier = "Iron";
+//                    break;
+//                case "Stone":
+//                    tier = "Stone";
+//                    break;
+//                case "Gold":
+//                    tier = "Gold";
+//                    break;
+//                case "Diamond":
+//                    tier = "Diamond";
+//                    break;
+//                case "Netherite":
+//                    tier = "Netherite";
+//                    break;
+//                default:
+//                    tier = "Wood";
+//                    break;
+//            }
+//        } catch (Exception e) {
+//            Loader.Toast(String.valueOf(e));
+//        }
+//
+//        createSword(name, icon, index, type, tier, extraData.contains("durability") ? extraData.getInteger("durability") : 0, extraData.contains("damage") ? extraData.getInteger("damage") : 0);
+//    }//NC_ItemsPtr
+
     @JavascriptInterface
     public static void onItemUse(long Item, long ItemStack, long Actor, int x, int y, int z, short d, float e, float f, float g) {
         boolean blacked = false;
