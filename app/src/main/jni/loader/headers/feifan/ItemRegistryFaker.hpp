5ad7aa6521128ce641aecb00a818da512b0543d2
OneFeiFan <3022504358@qq.com>
2024-07-30 21:04:56 +0800

    修改了一下编译脚本，恢复了一下宏

--- app/src/main/jni/loader/headers/feifan/ItemRegistryFaker.hpp
+++ app/src/main/jni/loader/headers/feifan/ItemRegistryFaker.hpp
@@ -5,21 +5,23 @@
 #include "ItemFaker.hpp"
 
 // fake区域
-#define Extern(type, method, args...) extern type (*fake_##method) (args)
+//#define Extern(type, method, args...) extern type (*fake_##method) (args)
 extern short (*fake_ItemRegistry_mMaxItemID);
-Extern(WeakPtr<Item>, ItemRegistry_registerItemShared, std::string const&, short&);
-Extern(WeakPtr<Item>, ItemRegistry_registerItemSharedForSword, std::string const &, short &,Item::Tier const&);
-Extern(WeakPtr<Item>, ItemRegistry_getItemById, short);
-Extern(WeakPtr<Item>, ItemRegistry_registerItem, SharedPtr<Item>);
-Extern(const char*, ItemRegistry_getNameFromLegacyID, short);
-//Extern(void, ItemRegistry_registerLegacyID, std::string const&, short);
+extern WeakPtr<Item> (*fake_ItemRegistry_registerItemShared) (std::string const&, short&);
+extern WeakPtr<Item> (*fake_ItemRegistry_registerItemSharedForSword) (std::string const &, short &, Item::Tier const&);
+extern WeakPtr<Item> (*fake_ItemRegistry_getItemById) (short);
+extern WeakPtr<Item> (*fake_ItemRegistry_registerItem) (SharedPtr<Item>);
+extern const char* (*fake_ItemRegistry_getNameFromLegacyID) (short);
+//extern void (*fake_ItemRegistry_registerLegacyID) (std::string const&, short);
+
 
 //hook区
-#define Extern(type, method, args...) extern type (*base_##method) (args)
-Extern(WeakPtr<Item>, ItemRegistry_registerItemShared, std::string const &, short &);
-Extern(int, ItemRegistry_registerItem, SharedPtr<Item>);
+//#define Extern(type, method, args...) extern type (*base_##method) (args)
+extern WeakPtr<Item> (*base_ItemRegistry_registerItemShared) (std::string const &, short &);
+extern int (*base_ItemRegistry_registerItem) (SharedPtr<Item>);
 
-#define Extern(type, method, args...) extern type method (args)
-Extern(WeakPtr<Item>, NC_ItemRegistry_registerItemShared, std::string const &, short &);
-Extern(int, NC_ItemRegistry_registerItem, SharedPtr<Item>);
+//#define Extern(type, method, args...) extern type method (args)
+extern WeakPtr<Item> NC_ItemRegistry_registerItemShared (std::string const &, short &);
+extern int NC_ItemRegistry_registerItem (SharedPtr<Item>);
+
 #endif
