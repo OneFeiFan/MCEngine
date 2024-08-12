4f15697ad499e86bd1f2074332c4f35666adfe94
OneFeiFan <3022504358@qq.com>
2024-08-12 20:31:32 +0800

    修改报错的方法，添加完整的JS运行环境

--- app/src/main/jni/loader/bridges/QuickJSBridge.hpp
+++ app/src/main/jni/loader/bridges/QuickJSBridge.hpp
@@ -13,83 +13,171 @@ extern "C" {
 
 namespace QuickJSBridge
 {
-    static JSRuntime* globalRuntime;
-    static JSContext* globalContext;
+    static JSRuntime *globalRuntime = nullptr;
+    static JSContext *globalContext = nullptr;
 
     static JSValue exception_val;
     static JSValue val;
-    static bool is_error;
-    static const char *stack;
+    static const char *stack = nullptr;
 
-    static JSContext* getContext() {
+    static JSContext *getContext()
+    {
         return globalContext;
     }
-    static JSRuntime* getRuntime() {
+
+    static JSRuntime *getRuntime()
+    {
         return globalRuntime;
     }
-    static JSValue GetException() {
+
+    static JSValue GetException()
+    {
         return exception_val;
     }
-    static JSValue GetExceptionStack () {
+
+    static JSValue GetExceptionStack()
+    {
         return val;
     }
 
-    static void installization() {
+    static JSContext *JS_NewCustomContext(JSRuntime *rt)
+    {
+        JSContext *ctx = JS_NewContextRaw(rt);
+        if(!ctx)
+            return nullptr;
+        JS_AddIntrinsicBaseObjects(ctx);
+        JS_AddIntrinsicDate(ctx);
+        JS_AddIntrinsicEval(ctx);
+        JS_AddIntrinsicStringNormalize(ctx);
+        JS_AddIntrinsicRegExp(ctx);
+        JS_AddIntrinsicJSON(ctx);
+        JS_AddIntrinsicProxy(ctx);
+        JS_AddIntrinsicMapSet(ctx);
+        JS_AddIntrinsicTypedArrays(ctx);
+        JS_AddIntrinsicPromise(ctx);
+        JS_AddIntrinsicBigInt(ctx);
+
+        js_init_module_os(ctx, "os");
+        js_init_module_std(ctx, "std");
+        return ctx;
+    }
+
+    static void installization()
+    {
         globalRuntime = JS_NewRuntime();
+        if(!globalRuntime){
+            std::cerr << "Failed to create JSRuntime" << std::endl;
+            return;
+        }
+        js_std_set_worker_new_context_func(JS_NewCustomContext);
         js_std_init_handlers(globalRuntime);
-        globalContext = JS_NewContext(globalRuntime);
+        JS_SetModuleLoaderFunc(globalRuntime, nullptr, js_module_loader, nullptr);
+        globalContext = JS_NewCustomContext(globalRuntime);
+        if(!globalContext){
+            std::cerr << "Failed to create JSContext" << std::endl;
+            JS_FreeRuntime(globalRuntime);
+            return;
+        }
+        js_std_add_helpers(globalContext, 0, nullptr);
+        const char *source = "import * as os from 'os';\n"
+                             "for(let key in os){\n"
+                             "globalThis[key] = os[key];"
+                             "}\n";
+        JSValue val1 = JS_Eval(globalContext, source, strlen(source), "<input>", JS_EVAL_TYPE_MODULE | JS_EVAL_FLAG_COMPILE_ONLY);
+        if(!JS_IsException(val1)){
+            js_module_set_import_meta(globalContext, val1, FALSE, TRUE);
+            val1 = JS_EvalFunction(globalContext, val1);
+            js_std_await(globalContext, val1);
+        }
+        JS_FreeValue(globalContext, val1);
 
         ItemBridge::installization(globalContext);
         UsefulToolBridge::installization(globalContext);
-
-        js_std_add_helpers(globalContext, 0, nullptr);
-        js_init_module_std(globalContext, "std");
-        js_init_module_os(globalContext, "os");
-
     }
-    static void eval(const char *input, size_t input_len, const char *filename) {
-        JSValue main = JS_Eval(globalContext, input, input_len, filename, JS_EVAL_TYPE_MODULE | JS_EVAL_FLAG_COMPILE_ONLY);
 
-        exception_val = JS_GetException(getContext());
-        is_error = JS_IsError(globalContext, exception_val);
-        if (!JS_IsException(main)) {
-            js_module_set_import_meta(globalContext, main, FALSE, TRUE);
-            main = JS_EvalFunction(globalContext, main);
-        } else {
-            val = JS_GetPropertyStr(globalContext, exception_val, "stack");
-            stack = JS_ToCString(globalContext, val);
-            printf("%s\n", stack);
-            printf("%s\n", JS_ToCString(globalContext, exception_val));
-            JS_FreeCString(globalContext, stack);
-            JS_FreeValue(globalContext, val);
-            JS_FreeValue(globalContext, exception_val);
+    static int eval_buf(JSContext *ctx, const char *buf, int buf_len, const char *filename, int eval_flags)
+    {
+        JSValue val;
+        int ret = 0;
+
+        if((eval_flags & JS_EVAL_TYPE_MASK) == JS_EVAL_TYPE_MODULE){
+            val = JS_Eval(ctx, buf, buf_len, filename, eval_flags | JS_EVAL_FLAG_COMPILE_ONLY);
+            if(!JS_IsException(val)){
+                js_module_set_import_meta(ctx, val, TRUE, TRUE);
+                val = JS_EvalFunction(ctx, val);
+                val = js_std_await(ctx, val);
+            }
+        }else {
+            val = JS_Eval(ctx, buf, buf_len, filename, eval_flags);
+        }
+
+        if(JS_IsException(val)){
+            val = JS_GetException(ctx);
+            const char *str = JS_ToCString(ctx, val);
+            if(str){
+                std::cout << str << std::endl;
+                JS_FreeCString(ctx, str);
+            }else {
+                std::cout << "未知异常" << std::endl;
+            }
+
+            if(JS_IsError(ctx, val)){
+                val = JS_GetPropertyStr(ctx, val, "stack");
+                if(!JS_IsUndefined(val)){
+                    str = JS_ToCString(ctx, val);
+                    if(str){
+                        std::cout << str << std::endl;
+                        JS_FreeCString(ctx, str);
+                    }else {
+                        std::cout << "未知异常" << std::endl;
+                    }
+                    JS_FreeValue(ctx, val);
+                }
+            }
+            ret = -1;
+        }
+
+        JS_FreeValue(ctx, val);
+        return ret;
+    }
+
+    static void eval(const char *input, size_t input_len, const char *filename)
+    {
+        int result = eval_buf(globalContext, input, input_len, filename, JS_EVAL_TYPE_MODULE);
+        js_std_loop(globalContext);
+        if(result != 0){
             JS_FreeContext(globalContext);
             JS_FreeRuntime(globalRuntime);
         }
     }
-    static void loadModules(const char *input, size_t input_len, const char *filename) {
+
+    static void loadModules(const char *input, size_t input_len, const char *filename)
+    {
         JSValue module = JS_Eval(globalContext, input, input_len, filename, JS_EVAL_TYPE_MODULE | JS_EVAL_FLAG_COMPILE_ONLY);
 
         exception_val = JS_GetException(getContext());
-        if (!JS_IsException(module)) {
+        if(!JS_IsException(module)){
             js_module_set_import_meta(globalContext, module, FALSE, TRUE);
             module = JS_EvalFunction(globalContext, module);
-        } else {
+            module = js_std_await(globalContext, module);
+        }else {
             val = JS_GetPropertyStr(globalContext, exception_val, "stack");
             stack = JS_ToCString(globalContext, val);
-            printf("%s\n", stack);
-            printf("%s\n", JS_ToCString(globalContext, exception_val));
-            JS_FreeCString(globalContext, stack);
+            if(stack){
+                std::cerr << stack << std::endl;
+                JS_FreeCString(globalContext, stack);
+            }
+            const char *exceptionStr = JS_ToCString(globalContext, exception_val);
+            if(exceptionStr){
+                std::cerr << exceptionStr << std::endl;
+                JS_FreeCString(globalContext, exceptionStr);
+            }
             JS_FreeValue(globalContext, val);
             JS_FreeValue(globalContext, exception_val);
             JS_FreeContext(globalContext);
             JS_FreeRuntime(globalRuntime);
         }
-        module = js_std_await(globalContext, module);
     }
-
 }
-
 }
-
 #endif //MCENGINE_QUICKJSBRIDGE_HPP
